6ee67a3cdac8ddb7eda0bce364a2b51740de818a
Antonio Hamilton <antoniohamiltonfreitas@outlook.com>
2020-04-02 14:31:52 -0300

    fix(cors): cors fixed

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
-  app.enableCors({
-    origin: '*',
-  });
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT);
+    app.enableCors();
 }
 bootstrap();
